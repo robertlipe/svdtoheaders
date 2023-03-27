b20ad5cc21fbdd9469d79532feea7bfc18d7b221
Robert Lipe <robertlipe@users.noreply.github.com>
2023-03-27 04:54:43 -0500

    Add SVD and converted headers for CH32V family
    People can poke at files online without downloading or confirm that their python install is working same as mine or whatever.

---
+++ demo/h/ch32v103_map.h
@@ -0,0 +1,33 @@
+#define CH32V103_ADC_BASE    0x40012400 /* 0x40012400-0x400127ff: 1kB Analog to digital converter */
+#define CH32V103_AFIO_BASE   0x40010000 /* 0x40010000-0x400103ff: 1kB Alternate function I/O */
+#define CH32V103_BKP_BASE    0x40006c00 /* 0x40006c00-0x40006fff: 1kB Backup registers */
+#define CH32V103_CRC_BASE    0x40023000 /* 0x40023000-0x400233ff: 1kB CRC calculation unit */
+#define CH32V103_DAC1_BASE   0x40007400 /* 0x40007400-0x400077ff: 1kB Digital to analog converter */
+#define CH32V103_DBG_BASE    0xe0042000 /* 0xe0042000-0xe00423ff: 1kB Debug support */
+#define CH32V103_DMA_BASE    0x40020000 /* 0x40020000-0x400203ff: 1kB DMA controller */
+#define CH32V103_EXTEND_BASE 0x40023800 /* 0x40023800-0x40023fff: 2kB extension configuration */
+#define CH32V103_EXTI_BASE   0x40010400 /* 0x40010400-0x400107ff: 1kB EXTI */
+#define CH32V103_FLASH_BASE  0x40022000 /* 0x40022000-0x400223ff: 1kB FLASH */
+#define CH32V103_GPIOA_BASE  0x40010800 /* 0x40010800-0x40010bff: 1kB General purpose I/O */
+#define CH32V103_GPIOB_BASE  0x40010c00 /* 0x40010c00-0x40010fff: 1kB GPIOB */
+#define CH32V103_GPIOC_BASE  0x40011000 /* 0x40011000-0x400113ff: 1kB GPIOC */
+#define CH32V103_GPIOD_BASE  0x40011400 /* 0x40011400-0x400117ff: 1kB GPIOD */
+#define CH32V103_I2C1_BASE   0x40005400 /* 0x40005400-0x400057ff: 1kB Inter integrated circuit */
+#define CH32V103_I2C2_BASE   0x40005800 /* 0x40005800-0x40005bff: 1kB I2C2 */
+#define CH32V103_IWDG_BASE   0x40003000 /* 0x40003000-0x400033ff: 1kB Independent watchdog */
+#define CH32V103_PFIC_BASE   0xe000e000 /* 0xe000e000-0xe000f0ff: 4kB Programmable Fast Interrupt Controller */
+#define CH32V103_PWR_BASE    0x40007000 /* 0x40007000-0x400073ff: 1kB Power control */
+#define CH32V103_RCC_BASE    0x40021000 /* 0x40021000-0x400213ff: 1kB Reset and clock control */
+#define CH32V103_RTC_BASE    0x40002800 /* 0x40002800-0x40002bff: 1kB Real time clock */
+#define CH32V103_SPI1_BASE   0x40013000 /* 0x40013000-0x400133ff: 1kB Serial peripheral interface */
+#define CH32V103_SPI2_BASE   0x40003800 /* 0x40003800-0x40003bff: 1kB SPI2 */
+#define CH32V103_TIM1_BASE   0x40012c00 /* 0x40012c00-0x40012fff: 1kB Advanced timer */
+#define CH32V103_TIM2_BASE   0x40000000 /* 0x40000000-0x400003ff: 1kB General purpose timer */
+#define CH32V103_TIM3_BASE   0x40000400 /* 0x40000400-0x400007ff: 1kB TIM3 */
+#define CH32V103_TIM4_BASE   0x40000800 /* 0x40000800-0x40000bff: 1kB TIM4 */
+#define CH32V103_USART1_BASE 0x40013800 /* 0x40013800-0x40013bff: 1kB Universal synchronous asynchronous receiver transmitter */
+#define CH32V103_USART2_BASE 0x40004400 /* 0x40004400-0x400047ff: 1kB USART2 */
+#define CH32V103_USART3_BASE 0x40004800 /* 0x40004800-0x40004bff: 1kB USART3 */
+#define CH32V103_USBD_BASE   0x40005c00 /* 0x40005c00-0x40005fff: 1kB Universal serial bus full-speed device interface */
+#define CH32V103_USBHD_BASE  0x40023400 /* 0x40023400-0x400237ff: 1kB USB register */
+#define CH32V103_WWDG_BASE   0x40002c00 /* 0x40002c00-0x40002fff: 1kB Window watchdog */
---
+++ demo/h/ch32v20x_map.h
@@ -0,0 +1,52 @@
+#define CH32V203_ADC1_BASE   0x40012400 /* 0x40012400-0x400127ff: 1kB Analog to digital converter */
+#define CH32V203_ADC2_BASE   0x40012800 /* 0x40012800-0x40012bff: 1kB Analog to digital converter */
+#define CH32V203_AFIO_BASE   0x40010000 /* 0x40010000-0x400103ff: 1kB Alternate function I/O */
+#define CH32V203_BKP_BASE    0x40006c00 /* 0x40006c00-0x40006fff: 1kB Backup registers */
+#define CH32V203_CAN1_BASE   0x40006400 /* 0x40006400-0x400067ff: 1kB Controller area network */
+#define CH32V203_CRC_BASE    0x40023000 /* 0x40023000-0x400233ff: 1kB CRC calculation unit */
+#define CH32V203_DAC_BASE    0x40007400 /* 0x40007400-0x400077ff: 1kB Digital to analog converter */
+#define CH32V203_DBG_BASE    0xe000d000 /* 0xe000d000-0xe000d3ff: 1kB Debug support */
+#define CH32V203_DMA1_BASE   0x40020000 /* 0x40020000-0x400203ff: 1kB DMA1 controller */
+#define CH32V203_ETHERNET_DMA_BASE 0x40029000 /* 0x40029000-0x400293ff: 1kB Ethernet: DMA controller operation */
+#define CH32V203_ETHERNET_MAC_BASE 0x40028000 /* 0x40028000-0x400283ff: 1kB Ethernet: media access control */
+#define CH32V203_ETHERNET_MMC_BASE 0x40028100 /* 0x40028100-0x400284ff: 1kB Ethernet: MAC management counters */
+#define CH32V203_ETHERNET_PTP_BASE 0x40028700 /* 0x40028700-0x40028aff: 1kB Ethernet: Precision time protocol */
+#define CH32V203_EXTEND_BASE 0x40023800 /* 0x40023800-0x40023bff: 1kB Extend configuration */
+#define CH32V203_EXTI_BASE   0x40010400 /* 0x40010400-0x400107ff: 1kB EXTI */
+#define CH32V203_FLASH_BASE  0x40022000 /* 0x40022000-0x400223ff: 1kB FLASH */
+#define CH32V203_GPIOA_BASE  0x40010800 /* 0x40010800-0x40010bff: 1kB General purpose I/O */
+#define CH32V203_GPIOB_BASE  0x40010c00 /* 0x40010c00-0x40010fff: 1kB GPIOB */
+#define CH32V203_GPIOC_BASE  0x40011000 /* 0x40011000-0x400113ff: 1kB GPIOC */
+#define CH32V203_GPIOD_BASE  0x40011400 /* 0x40011400-0x400117ff: 1kB GPIOD */
+#define CH32V203_GPIOE_BASE  0x40011800 /* 0x40011800-0x40011bff: 1kB GPIOE */
+#define CH32V203_I2C1_BASE   0x40005400 /* 0x40005400-0x400057ff: 1kB Inter integrated circuit */
+#define CH32V203_I2C2_BASE   0x40005800 /* 0x40005800-0x40005bff: 1kB I2C2 */
+#define CH32V203_IWDG_BASE   0x40003000 /* 0x40003000-0x400033ff: 1kB Independent watchdog */
+#define CH32V203_OPA_BASE    0x40023804 /* 0x40023804-0x40023a03: 1kB OPA configuration */
+#define CH32V203_PFIC_BASE   0xe000e000 /* 0xe000e000-0xe000f0ff: 4kB Programmable Fast Interrupt Controller */
+#define CH32V203_PWR_BASE    0x40007000 /* 0x40007000-0x400073ff: 1kB Power control */
+#define CH32V203_RCC_BASE    0x40021000 /* 0x40021000-0x400213ff: 1kB Reset and clock control */
+#define CH32V203_RTC_BASE    0x40002800 /* 0x40002800-0x40002bff: 1kB Real time clock */
+#define CH32V203_SPI1_BASE   0x40013000 /* 0x40013000-0x400133ff: 1kB Serial peripheral interface */
+#define CH32V203_SPI2_BASE   0x40003800 /* 0x40003800-0x40003bff: 1kB Serial peripheral interface */
+#define CH32V203_SPI3_BASE   0x40003c00 /* 0x40003c00-0x40003fff: 1kB SPI3 */
+#define CH32V203_TIM10_BASE  0x40015000 /* 0x40015000-0x400153ff: 1kB TIM10 */
+#define CH32V203_TIM1_BASE   0x40012c00 /* 0x40012c00-0x40012fff: 1kB Advanced timer */
+#define CH32V203_TIM2_BASE   0x40000000 /* 0x40000000-0x400003ff: 1kB General purpose timer */
+#define CH32V203_TIM3_BASE   0x40000400 /* 0x40000400-0x400007ff: 1kB TIM3 */
+#define CH32V203_TIM4_BASE   0x40000800 /* 0x40000800-0x40000bff: 1kB TIM4 */
+#define CH32V203_TIM5_BASE   0x40000c00 /* 0x40000c00-0x40000fff: 1kB TIM5 */
+#define CH32V203_TIM8_BASE   0x40013400 /* 0x40013400-0x400137ff: 1kB TIM8 */
+#define CH32V203_TIM9_BASE   0x40014c00 /* 0x40014c00-0x40014fff: 1kB TIM9 */
+#define CH32V203_UART4_BASE  0x40004c00 /* 0x40004c00-0x40004fff: 1kB UART4 */
+#define CH32V203_UART5_BASE  0x40005000 /* 0x40005000-0x400053ff: 1kB UART5 */
+#define CH32V203_UART6_BASE  0x40001800 /* 0x40001800-0x40001bff: 1kB UART6 */
+#define CH32V203_UART7_BASE  0x40001c00 /* 0x40001c00-0x40001fff: 1kB UART7 */
+#define CH32V203_UART8_BASE  0x40002000 /* 0x40002000-0x400023ff: 1kB UART8 */
+#define CH32V203_USART1_BASE 0x40013800 /* 0x40013800-0x40013bff: 1kB Universal synchronous asynchronous receiver transmitter */
+#define CH32V203_USART2_BASE 0x40004400 /* 0x40004400-0x400047ff: 1kB USART2 */
+#define CH32V203_USART3_BASE 0x40004800 /* 0x40004800-0x40004bff: 1kB USART3 */
+#define CH32V203_USBHD_BASE  0x40023400 /* 0x40023400-0x400237ff: 1kB USB register */
+#define CH32V203_USB_BASE    0x40005c00 /* 0x40005c00-0x40005fff: 1kB Universal serial bus full-speed device interface */
+#define CH32V203_USB_OTG_FS_BASE 0x50000000 /* 0x50000000-0x5003ffff: 256kB USB FS OTG register */
+#define CH32V203_WWDG_BASE   0x40002c00 /* 0x40002c00-0x40002fff: 1kB Window watchdog */
