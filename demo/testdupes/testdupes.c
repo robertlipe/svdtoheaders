97f7814244ca164193fcf86e262a46b8918d733c
Robert Lipe <robertlipe@gmail.com>
2023-03-27 13:44:44 -0500

    Add retest to rebuild everything easily

---
+++ demo/testdupes/testdupes.c
@@ -0,0 +1,13 @@
+#include "../h/ch32v003_map.h"
+#include "../h/ch32v003_reg.h"
+#include "../h/ch32v103_map.h"
+#include "../h/ch32v103_reg.h"
+#include "../h/ch32v20x_map.h"
+#include "../h/ch32v20x_reg.h"
+#include "../h/ch32v307_map.h"
+#include "../h/ch32v307_reg.h"
+
+
+// #define CH32V_PFIC_STK_CMPHR_OFFSET 4321
+long foo() { return CH32V_PFIC_STK_CMPHR_CMPH_MASK; }
+
