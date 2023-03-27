e309dba2bf3a2349aa5126afe0e6373efad4adf7
Robert Lipe <robertlipe@gmail.com>
2023-03-27 13:41:46 -0500

    Add ch32v003

---
+++ demo/h/ch32v003_map.h
@@ -0,0 +1,21 @@
+#define CH32V003_ADC1_BASE   0x40012400 /* 0x40012400-0x400127ff: 1kB Analog to digital converter */
+#define CH32V003_AFIO_BASE   0x40010000 /* 0x40010000-0x400103ff: 1kB Alternate function I/O */
+#define CH32V003_DBG_BASE    0xe000d000 /* 0xe000d000-0xe000d3ff: 1kB Debug support */
+#define CH32V003_DMA1_BASE   0x40020000 /* 0x40020000-0x400203ff: 1kB DMA1 controller */
+#define CH32V003_ESIG_BASE   0x1ffff7e0 /* 0x1ffff7e0-0x1ffff7f3: 1kB Device electronic signature */
+#define CH32V003_EXTEND_BASE 0x40023800 /* 0x40023800-0x40023bff: 1kB Extend configuration */
+#define CH32V003_EXTI_BASE   0x40010400 /* 0x40010400-0x400107ff: 1kB EXTI */
+#define CH32V003_FLASH_BASE  0x40022000 /* 0x40022000-0x400223ff: 1kB FLASH */
+#define CH32V003_GPIOA_BASE  0x40010800 /* 0x40010800-0x40010bff: 1kB General purpose I/O */
+#define CH32V003_GPIOC_BASE  0x40011000 /* 0x40011000-0x400113ff: 1kB GPIOC */
+#define CH32V003_GPIOD_BASE  0x40011400 /* 0x40011400-0x400117ff: 1kB GPIOD */
+#define CH32V003_I2C1_BASE   0x40005400 /* 0x40005400-0x400057ff: 1kB Inter integrated circuit */
+#define CH32V003_IWDG_BASE   0x40003000 /* 0x40003000-0x400033ff: 1kB Independent watchdog */
+#define CH32V003_PFIC_BASE   0xe000e000 /* 0xe000e000-0xe000f0ff: 4kB Programmable Fast Interrupt Controller */
+#define CH32V003_PWR_BASE    0x40007000 /* 0x40007000-0x400073ff: 1kB Power control */
+#define CH32V003_RCC_BASE    0x40021000 /* 0x40021000-0x400213ff: 1kB Reset and clock control */
+#define CH32V003_SPI1_BASE   0x40013000 /* 0x40013000-0x400133ff: 1kB Serial peripheral interface */
+#define CH32V003_TIM1_BASE   0x40012c00 /* 0x40012c00-0x40012fff: 1kB Advanced timer */
+#define CH32V003_TIM2_BASE   0x40000000 /* 0x40000000-0x400003ff: 1kB General purpose timer */
+#define CH32V003_USART1_BASE 0x40013800 /* 0x40013800-0x40013bff: 1kB Universal synchronous asynchronous receiver transmitter */
+#define CH32V003_WWDG_BASE   0x40002c00 /* 0x40002c00-0x40002fff: 1kB Window watchdog */
